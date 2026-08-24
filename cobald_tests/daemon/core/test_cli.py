609e35bb0a9a033bc34d579675e0bcf5f3bb5a11
Max Kühn <maxfischer2781@gmail.com>
2026-08-24 13:58:05 +0200

    black

--- cobald_tests/daemon/core/test_cli.py
+++ cobald_tests/daemon/core/test_cli.py
@@ -16,8 +16,8 @@ def test_daemon_cli():
                 """)
         start_time = time.monotonic()
         subprocess.check_call(
-            [sys.executable, '-m', 'cobald.daemon', config.name, '--timeout', '0.5']
+            [sys.executable, "-m", "cobald.daemon", config.name, "--timeout", "0.5"]
         )
-        duration = time.monotonic() -  start_time
+        duration = time.monotonic() - start_time
         assert duration >= 0.5, "daemon ran shorter than expected"
         assert duration <= 1.0, "daemon ran longer than expected"
